87644be7451a40b2f97b759f48523f4d7f5120f3
Mateo Gianolio <gianoliomateo@gmail.com>
2016-12-09 00:31:40 +0100

    fix #7: info[5] -> info[4]

--- routines/level1/rotmg.cc
+++ routines/level1/rotmg.cc
@@ -6,7 +6,7 @@ void drotmg(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	double *d2 = reinterpret_cast<double*>(GET_CONTENTS(info[1].As<v8::Float64Array>()));
 	double *b1 = reinterpret_cast<double*>(GET_CONTENTS(info[2].As<v8::Float64Array>()));
 	const double b2 = info[3]->NumberValue();
-	double *param = reinterpret_cast<double*>(GET_CONTENTS(info[5].As<v8::Float64Array>()));
+	double *param = reinterpret_cast<double*>(GET_CONTENTS(info[4].As<v8::Float64Array>()));
 	cblas_drotmg(d1, d2, b1, b2, param);
 }
 
@@ -15,6 +15,6 @@ void srotmg(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	float *d2 = reinterpret_cast<float*>(GET_CONTENTS(info[1].As<v8::Float32Array>()));
 	float *b1 = reinterpret_cast<float*>(GET_CONTENTS(info[2].As<v8::Float32Array>()));
 	const float b2 = info[3]->NumberValue();
-	float *param = reinterpret_cast<float*>(GET_CONTENTS(info[5].As<v8::Float32Array>()));
+	float *param = reinterpret_cast<float*>(GET_CONTENTS(info[4].As<v8::Float32Array>()));
 	cblas_srotmg(d1, d2, b1, b2, param);
 }
