47023326eab6e3f73a8d9a2c00775b0837f5e5dc
Mateo Gianolio <gianoliomateo@gmail.com>
2023-04-18 00:22:58 +0200

    Add missing GET_CONTENTS calls

--- routines/level3/symm.cc
+++ routines/level3/symm.cc
@@ -12,7 +12,7 @@ void dsymm(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	const double *b = reinterpret_cast<double*>(GET_CONTENTS(info[7].As<v8::Float64Array>()));
 	const int ldb = info[8].As<v8::Integer>()->Value();
 	const double beta = info[9].As<v8::Number>()->Value();
-	double *c = reinterpret_cast<double*>(info[10].As<v8::Float64Array>()->Buffer()->GetContents().Data());
+	double *c = reinterpret_cast<double*>(GET_CONTENTS(info[10].As<v8::Float64Array>()));
 	const int ldc = info[11].As<v8::Integer>()->Value();
 	cblas_dsymm(CblasRowMajor, side, uplo, m, n, alpha, a, lda, b, ldb, beta, c, ldc);
 }
@@ -28,7 +28,7 @@ void ssymm(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	const float *b = reinterpret_cast<float*>(GET_CONTENTS(info[7].As<v8::Float32Array>()));
 	const int ldb = info[8].As<v8::Integer>()->Value();
 	const float beta = info[9].As<v8::Number>()->Value();
-	float *c = reinterpret_cast<float*>(info[10].As<v8::Float32Array>()->Buffer()->GetContents().Data());
+	float *c = reinterpret_cast<float*>(GET_CONTENTS(info[10].As<v8::Float32Array>()));
 	const int ldc = info[11].As<v8::Integer>()->Value();
 	cblas_ssymm(CblasRowMajor, side, uplo, m, n, alpha, a, lda, b, ldb, beta, c, ldc);
 }
@@ -43,8 +43,8 @@ void zsymm(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	const int lda = info[6].As<v8::Integer>()->Value();
 	const double *b = reinterpret_cast<double*>(GET_CONTENTS(info[7].As<v8::Float64Array>()));
 	const int ldb = info[8].As<v8::Integer>()->Value();
-	const double *beta = reinterpret_cast<double*>(info[9].As<v8::Float64Array>()->Buffer()->GetContents().Data());
-	double *c = reinterpret_cast<double*>(info[10].As<v8::Float64Array>()->Buffer()->GetContents().Data());
+	const double *beta = reinterpret_cast<double*>(GET_CONTENTS(info[9].As<v8::Float64Array>()));
+	double *c = reinterpret_cast<double*>(GET_CONTENTS(info[10].As<v8::Float64Array>()));
 	const int ldc = info[11].As<v8::Integer>()->Value();
 	cblas_zsymm(CblasRowMajor, side, uplo, m, n, alpha, a, lda, b, ldb, beta, c, ldc);
 }
@@ -59,8 +59,8 @@ void csymm(const v8::FunctionCallbackInfo<v8::Value>& info) {
 	const int lda = info[6].As<v8::Integer>()->Value();
 	const float *b = reinterpret_cast<float*>(GET_CONTENTS(info[7].As<v8::Float32Array>()));
 	const int ldb = info[8].As<v8::Integer>()->Value();
-	const float *beta = reinterpret_cast<float*>(info[9].As<v8::Float32Array>()->Buffer()->GetContents().Data());
-	float *c = reinterpret_cast<float*>(info[10].As<v8::Float32Array>()->Buffer()->GetContents().Data());
+	const float *beta = reinterpret_cast<float*>(GET_CONTENTS(info[9].As<v8::Float32Array>()));
+	float *c = reinterpret_cast<float*>(GET_CONTENTS(info[10].As<v8::Float32Array>()));
 	const int ldc = info[11].As<v8::Integer>()->Value();
 	cblas_csymm(CblasRowMajor, side, uplo, m, n, alpha, a, lda, b, ldb, beta, c, ldc);
 }
